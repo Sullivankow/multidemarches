99f18df2e3805f3f2166fc42363c22efde9e7f4f
SunDev <sundly@live.fr>
2025-08-10 15:17:39 +0200

    Added header + hero + navbar

---
+++ template/header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="/asset/css/override-bootstrap.css">
+
+    <title>Multi-Démarches, l'assistance pour vos démarches administratives</title>
+</head>
+
+
+
+
+<body>
+
+
+
+    <header class="hero-header">
+        <div class="hero-overlay"></div>
+        <nav class="navbar navbar-expand-lg navbar-custom px-4 pt-3">
+            <a class="navbar-brand d-flex align-items-center gap-2" href="/">
+                <img src="/asset/images/logo.png" alt="Logo Multi-Démarches" width="48" height="48" style="object-fit:contain;">
+                <span class="fw-bold mb-2 text-white" style="font-size: 1.3rem; margin-left: 0.5rem;">Multi-Démarches</span>
+            </a>
+            <button class=" navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                    <li class="nav-item ">
+                        <a class="nav-link active text-white" aria-current="page" href="#">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#services">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#contact">Contact</a>
+                    </li>
+                </ul>
+                <a href="#rdv" class="btn btn-rdv ms-lg-3 mt-2 mt-lg-0">Prendre rendez-vous</a>
+            </div>
+        </nav>
+        <div class="hero-content container d-flex flex-column justify-content-center align-items-center">
+            <h1 class="hero-title">Multi-Démarches : Assistance administrative simplifiée</h1>
+            <p class="hero-text">Gagnez du temps, évitez les erreurs et laissez-vous guider dans toutes vos démarches administratives. Un accompagnement humain, rapide et efficace !</p>
+            <a href="#services" class="btn btn-primary btn-lg">Voir les services</a>
+        </div>
+    </header>
+
+
+
+
+
+
+
+
+    <main>
